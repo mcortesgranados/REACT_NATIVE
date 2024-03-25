f5c8f53423b3eba5dfc881dda779cbccc131fd36
Manuela Cortés Granados <manuelcortesgranados@gmail.com>
2024-03-25 10:44:07 -0500

    REACT_NATIVE\01_BASIC\01_03_State Management\state-management Comments added

--- 01_BASIC/01_03_State Management/state-management/src/ToggleButton.js
+++ 01_BASIC/01_03_State Management/state-management/src/ToggleButton.js
@@ -1,3 +1,15 @@
+/*
+In the provided code, React applies state management in the ToggleButton component. Specifically, it uses the useState hook to manage the state
+ of the toggle button. Here's the relevant part of the code:
+ 
+ // Define state for the toggle button
+const [isToggled, setIsToggled] = useState(false);
+
+
+This line initializes a state variable isToggled with an initial value of false, and a function setIsToggled to update the state. 
+React's useState hook allows functional components to manage state without using class components.
+
+*/
 // Author: Manuela Cortés Granados
 // Location: Bogotá DC, Colombia
 // Date: March 7, 2024
